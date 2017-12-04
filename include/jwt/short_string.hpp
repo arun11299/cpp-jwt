de38b3a82a923861e959f26762d39373daf4e6af
Arun M <arun11299@gmail.com>
2017-12-04 13:23:08 +0530

    Added short string type

---
+++ include/jwt/short_string.hpp
@@ -0,0 +1,14 @@
+#ifndef CPP_JWT_SHORT_STRING_HPP
+#define CPP_JWT_SHORT_STRING_HPP
+
+#include <string>
+#include "jwt/stack_alloc.hpp"
+
+namespace jwt {
+
+template <size_t N>
+using short_string = std::basic_string<char, std::char_traits<char>, stack_alloc<char, N>>;
+
+}
+
+#endif
