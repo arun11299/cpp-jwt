5b49fc57b6ec5f4918ccafc485b391f659c7cc62
Matt Frantz <matthew.h.frantz@gmail.com>
2018-10-24 17:40:11 -0700

    issue-33: Add test showing how to invoke move ctor

---
+++ tests/test_jwt_object.cc
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "jwt/jwt.hpp"
+
+namespace {
+
+struct Wrapper
+{
+    // The std::move here is required to resolve to the move ctor
+    // rather than to the universal reference ctor.
+    Wrapper(jwt::jwt_object&& obj) : object{std::move(obj)} {}
+    jwt::jwt_object object;
+};
+
+} // END namespace
+
+TEST (ObjectTest, MoveConstructor)
+{
+  using namespace jwt::params;
+
+  jwt::jwt_object obj{algorithm("hs256"), secret("secret")};
+
+  obj.add_claim("iss", "arun.muralidharan");
+
+  auto wrapper = Wrapper{std::move(obj)};
+
+  EXPECT_EQ(wrapper.object.header().algo(), jwt::algorithm::HS256);
+  EXPECT_EQ(wrapper.object.secret(), "secret");
+  EXPECT_TRUE(wrapper.object.payload().has_claim_with_value("iss", "arun.muralidharan"));
+}
+
+int main(int argc, char **argv) 
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
